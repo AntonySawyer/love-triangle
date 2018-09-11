5d912161ac8dc1a00fc40534ef68a839b318fa72
Antony <A.Krutenok@gmail.com>
2018-09-11 16:02:31 +0300

    add feature

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var count = 0;
+	for (var i = 0; i < preferences.length; i++) {
+/* firstPosition - starts with 1, second and third positions = first and second values*/
+		var firstValue = preferences[i]; 
+		var secondValue = preferences[firstValue - 1];  
+		var thirdValue = preferences[secondValue - 1]; 
+		var firstPosition = i + 1; 
+
+		if (firstValue != 0 && secondValue != 0 && thirdValue != 0 
+			&& thirdValue == firstPosition 
+			&& firstPosition != firstValue && firstValue != secondValue) {
+			count ++;
+/* rewrite to exclude re-use (first line in if statment) */
+			preferences[firstPosition-1] = 0;
+			preferences[firstValue-1] = 0;
+			preferences[secondValue - 1] = 0;
+		}			
+	}
+	return count;
 };
